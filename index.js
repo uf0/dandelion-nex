50a76a2c3fd275ae76386d3fd6df7df15a825398
uf0 <scnepa222@gmail.com>
2017-06-25 16:48:41 +0200

    fetch url and save results

--- index.js
+++ index.js
@@ -1,13 +1,70 @@
 var MongoClient = require('mongodb').MongoClient,
     request = require('request'),
+    async = require('async'),
+    fs = require('fs'),
+    d3dsv = require('d3-dsv'),
     config = require('./config.js');
 
-// Connection URL
 var url = 'mongodb://localhost:27017/mozilla';
-// Use connect method to connect to the Server
+var endpoint = 'https://api.dandelion.eu/datatxt/nex/v1';
+
 MongoClient.connect(url, function(err, db) {
   if(err){throw err}
-  console.log("Connected correctly to server");
+  var collection = db.collection('dandelion-nex');
 
-  db.close();
+  // load data
+  var data = d3dsv.tsvParse(fs.readFileSync('data/urls.tsv', 'utf-8'));
+  var total = data.length;
+
+
+  async.eachOfSeries(
+    data,
+    function(page, index, callback) {
+
+        getEntity(page.url, function(err,response){
+          if(err){
+            console.log('ERROR ' + (index+1) + '/' + total + ' ' + page.url, err);
+            callback()
+          }else{
+            if(response.error){
+              console.log('ERROR ' + (index+1) + '/' + total + ' ' + page.url, response);
+              callback()
+            }else{
+              response.source = page;
+              collection.insertOne(response, function(err, result) {
+                  if(err){
+                    console.log('ERROR ' + (index+1) + '/' + total + ' ' + page.url, err);
+                    callback()
+                  }else{
+                    console.log((index+1) + '/' + total + ' ' + page.url);
+                    callback()
+                  }
+                });
+            }
+          }
+        })
+
+      },
+    function(err) {
+        if( err ) {
+          console.log(err);
+        } else {
+          console.log('All urls have been processed successfully');
+          db.close();
+        }
+      }
+  );
 });
+
+
+
+function getEntity(url, callback){
+  var params = {
+    url:url,
+    token: config.token,
+    include: 'types'
+  }
+  request({url:endpoint, qs:params}, function(err, response, body) {
+    callback(err, JSON.parse(body))
+  });
+}
